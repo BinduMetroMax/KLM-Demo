25c8fc81f3330c843d782eba9b0240a4f6f1e23b
BinduMetroMax <bindu@themetromaxgroup.com>
2023-10-11 08:06:19 +0530

    Initial commit from create-refine-app

---
+++ pages/_app.tsx
@@ -0,0 +1,78 @@
+import {
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  notificationProvider,
+} from "@refinedev/antd";
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Header } from "@components/header";
+import { ColorModeContextProvider } from "@contexts";
+import "@refinedev/antd/dist/reset.css";
+import dataProvider from "@refinedev/simple-rest";
+import { authProvider } from "src/authProvider";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <ThemedLayoutV2
+        Header={() => <Header sticky />}
+        Sider={(props) => <ThemedSiderV2 {...props} fixed />}
+      >
+        <Component {...pageProps} />
+      </ThemedLayoutV2>
+    );
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <DevtoolsProvider>
+            <Refine
+              routerProvider={routerProvider}
+              dataProvider={dataProvider(API_URL)}
+              notificationProvider={notificationProvider}
+              authProvider={authProvider}
+              options={{
+                syncWithLocation: true,
+                warnWhenUnsavedChanges: true,
+                projectId: "fS7veK-nJkHnl-tYa1r9",
+              }}
+            >
+              {renderComponent()}
+              <RefineKbar />
+              <UnsavedChangesNotifier />
+              <DocumentTitleHandler />
+            </Refine>
+            <DevtoolsPanel />
+          </DevtoolsProvider>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default MyApp;
