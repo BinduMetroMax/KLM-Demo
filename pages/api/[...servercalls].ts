0a628f41b060fdf968d3eb4b0ece030d86ca9ba8
BinduMetroMax <bindu@themetromaxgroup.com>
2023-10-25 20:47:04 +0530

    fourth push

--- pages/api/[...servercalls].ts
+++ pages/api/[...servercalls].ts
@@ -1,43 +1,89 @@
 import axios, { AxiosInstance } from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import parseCookie from "utils/cookieParser";
+import jwt_decode from "jwt-decode";
 
-const backendApiBaseUrl = "https://dev-api.lmd.innowyze.in";
+
+
+
+const backendApiBaseUrl = "http://localhost:4005";
 let access_token: string;
 let refresh_token: string;
 
-const apiWithAuth: AxiosInstance = axios.create({
+const apiAxiosInstance: AxiosInstance = axios.create({
     baseURL: backendApiBaseUrl,
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
         Authorization: "",
     },
 });
 
-async function refreshAccessToken(refreshToken: string) {
-    await axios.post("/auth/refresh", {
-        refresh_token: refreshToken,
-    });
-    apiWithAuth.defaults.headers['Authorization'] = `Bearer ${access_token}`;
-}
 
-export default async function server_calls(req: NextApiRequest, res: NextApiResponse) {
+const getUpdatedAccesstoken = async (refreshToken: string) => {
+
+    let isRefreshTokenExpired = true;
+
+    if (isRefreshTokenExpired) {
+        const { exp }: any = jwt_decode(refreshToken);
+        isRefreshTokenExpired = Math.floor(Date.now() / 1000) > exp;
+        console.log(isRefreshTokenExpired);
+
+    } else {
+        const response = await apiAxiosInstance.get("/admin/auth/refresh-token", {
+            headers: {
+                Authorization: "Bearer " + refresh_token,
+            },
+        });
+
+        console.log(response.headers);
+    }
+};
+
+export default async function server_calls(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    console.log(req);
 
     const tokenFromCookie = parseCookie(req.headers.cookie);
 
-    access_token = tokenFromCookie.access_token
-    refresh_token = tokenFromCookie.refresh_token
+    console.log(tokenFromCookie);
+
+    access_token = tokenFromCookie.access_token;
+    refresh_token = tokenFromCookie.refresh_token;
+
+    console.log(access_token);
+    console.log(refresh_token);
 
     try {
+        let isAccessTokenExpired = true;
+
         const url: string = req.url!.replace("/api", "");
 
-        apiWithAuth.defaults.headers['Authorization'] = `Bearer ${access_token}`;
+        if (access_token) {
 
-        const axiosResponse = await apiWithAuth.request({
+            const { exp }: any = jwt_decode(access_token);
+            isAccessTokenExpired = Math.floor(Date.now() / 1000) > exp;
+            console.log(isAccessTokenExpired);
+        }
+
+        if (req.url !== "/admin/auth/login") {
+
+            if (isAccessTokenExpired) {
+                apiAxiosInstance.defaults.headers["Authorization"] = `Bearer ${getUpdatedAccesstoken(refresh_token)}`;
+            } else {
+                apiAxiosInstance.defaults.headers["Authorization"] = `Bearer ${access_token}`;
+            }
+
+        }
+
+        const axiosResponse = await apiAxiosInstance.request({
             url,
             method: req.method,
             data: req.body,
         });
+
+        console.log(axiosResponse.data);
 
         if (axiosResponse.headers["set-cookie"]) {
             res.setHeader("Set-Cookie", axiosResponse.headers["set-cookie"]);
@@ -45,19 +91,8 @@ export default async function server_calls(req: NextApiRequest, res: NextApiResp
 
         res.status(axiosResponse.status).json(axiosResponse.data);
 
-    } catch (err: any) {
-        if (err.response?.status === 401) {
-            try {
-                await refreshAccessToken(refresh_token);
-                const response = await apiWithAuth.request(err.config);
-                res.status(response.status).json(response.data);
-            } catch (refreshError) {
-                res.status(401).json("Access token and refresh token are invalid.");
-            }
-        } else {
-            res
-                .status(err.response?.status || 500)
-                .json(err.response?.data || "An error occurred");
-        }
+    } catch (error: any) {
+
+        res.status(error.response?.status).json(error.response?.data);
     }
 }
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -2,19 +2,15 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 function App() {
-    const [phoneNumber, setPhoneNumber] = useState<string>('');
-    const [code, setcodep] = useState<string>('');
-    const [step, setStep] = useState<number>(1); // 1 for phone number, 2 for OTP
+    const [mobile, setMobile] = useState<string>("8050692348");
+    const [otp, setOtp] = useState<string>('');
 
     const handleSendOtp = () => {
-        console.log(phoneNumber);
+        console.log(mobile);
         try {
-            axios.post("/api/auth/otp-login", {
-                phoneNumber: "1234567890"
+            axios.post("/api/admin/auth/login", {
+                mobile
             })
-            setTimeout(() => {
-                setStep(2); // Switch to the OTP step
-            }, 1000);
         } catch (error) {
             console.log(error, "PHONE ERROR");
         }
@@ -22,10 +18,12 @@ function App() {
 
     const handleVerifyOtp = () => {
         try {
-            console.log(code);
-            axios.post("/api/auth/validate", {
-                code: "123456",
-                phone: "1234567890"
+            console.log(otp);
+            axios.post("/api/admin/auth/validate", {
+                otp: otp,
+                mobile: "8050692348",
+                forAdmin: true,
+                loginSource: "adminWeb"
             })
         } catch (error) {
             console.log(error, "OTP ERROR");
@@ -33,39 +31,28 @@ function App() {
 
     };
 
-    const apiCallthwithCookie = () => {
-        axios.get("/api/dashboard", {
-            withCredentials: false
-        }).then((res) => console.log(res, "USERS DATA"))
-    }
-
     return (
         <div className="App">
-            {step === 1 && (
-                <div>
-                    <h2>Enter Phone Number</h2>
-                    <input
-                        type="text"
-                        placeholder="Phone Number"
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-                    <button onClick={handleSendOtp}>Send OTP</button>
-                </div>
-            )}
-            {step === 2 && (
-                <div>
-                    <h2>Enter OTP</h2>
-                    <input
-                        type="text"
-                        placeholder="OTP"
-                        value={code}
-                        onChange={(e) => setcodep(e.target.value)}
-                    />
-                    <button onClick={handleVerifyOtp}>Verify OTP</button>
-                </div>
-            )}
-            <button onClick={apiCallthwithCookie}>Call Api</button>
+            <div>
+                <h2>Enter Phone Number</h2>
+                <input
+                    type="text"
+                    placeholder="Phone Number"
+                    value={mobile}
+                    onChange={(e) => setMobile(e.target.value)}
+                />
+                <button onClick={handleSendOtp}>Send OTP</button>
+            </div>
+            <div>
+                <h2>Enter OTP</h2>
+                <input
+                    type="text"
+                    placeholder="OTP"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                />
+                <button onClick={handleVerifyOtp}>Verify OTP</button>
+            </div>
         </div>
 
 
